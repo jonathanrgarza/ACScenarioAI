e01378c4e26fa725e4216a5863785dbb4bde056b
Jonathan Garza <83787061+jonathanrgarza@users.noreply.github.com>
2021-12-12 22:44:31 -0600

    Update setup.py to include install_requires

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
+import os
 from setuptools import setup, find_namespace_packages
 
 packages = find_namespace_packages(where="src", include=['ac_carrier_scenario.*'])
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+requirements_path = current_directory + '/requirements.txt'
+install_requires = []
+if os.path.isfile(requirements_path):
+    with open(requirements_path) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name='AC_Carrier_Scenario',
@@ -11,5 +19,6 @@ setup(
     license='',
     author='Jonathan Garza',
     author_email='',
-    description='AI Project related to an AC Carrier Scenario'
+    description='AI Project related to an AC Carrier Scenario',
+    install_requires=install_requires
 )
